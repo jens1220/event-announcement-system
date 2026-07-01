8ce0e37b63f83b0f1f0677aa92bc135eeb6a2d4c
Jens <jeasbasd1220@gmail.com>
2026-07-01 11:49:35 +0800

    CICD Practice

--- frontend/app.js
+++ frontend/app.js
@@ -3,7 +3,7 @@
 // =====================================
 
 const API_URL =
-  "https://ia43lrde22.execute-api.ap-southeast-1.amazonaws.com/prod";
+  "https://gyh4tu0xn2.execute-api.ap-southeast-1.amazonaws.com/prod";
 
 
 // =====================================
