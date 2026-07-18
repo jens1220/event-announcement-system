ec1a7d8fda8aae705aecf17d36bec0575a729045
Jens <jeasbasd1220@gmail.com>
2026-07-18 15:10:42 +0800

    AnnouncementCheckerUpdate

--- lamba/AnnouncementCheckerLambda.py
+++ lamba/AnnouncementCheckerLambda.py
@@ -1,20 +1,19 @@
 import json
 import boto3
-from datetime import datetime
+from datetime import datetime, timedelta
 
 s3 = boto3.client("s3")
 sns = boto3.client("sns")
 
-BUCKET_NAME = "event-announcement-website-1220"
+BUCKET_NAME = "eas-demo-bucket"
 EVENTS_FILE = "frontend/events.json"
 
-TOPIC_ARN = "arn:aws:sns:ap-southeast-1:312695118485:event-announcements"
+TOPIC_ARN = "arn:aws:sns:ap-southeast-1:312695118485:EAS_SNS_Demo"
 
 
 def lambda_handler(event, context):
-    
-    print("AnnouncementCheckerLambda started")
-    print(event)
+
+    print("========== Announcement Checker Started ==========")
 
     response = s3.get_object(
         Bucket=BUCKET_NAME,
@@ -25,15 +24,25 @@ def lambda_handler(event, context):
         response["Body"].read().decode("utf-8")
     )
 
-    from datetime import datetime, timedelta
+    print("Events loaded:")
+    print(json.dumps(events, indent=2))
 
+    # Philippines Time (UTC+8)
     current_time = datetime.utcnow() + timedelta(hours=8)
+
+    print("Current Time:", current_time)
 
     updated = False
 
     for event_item in events:
 
+        print("----------------------------------")
+        print("Checking event:")
+        print(event_item)
+
         if event_item.get("announced"):
+
+            print("Already announced. Skipping.")
             continue
 
         event_datetime = datetime.strptime(
@@ -41,9 +50,13 @@ def lambda_handler(event, context):
             "%Y-%m-%d %H:%M"
         )
 
+        print("Event Time :", event_datetime)
+
         if current_time >= event_datetime:
 
-            sns.publish(
+            print("Event time reached! Sending SNS...")
+
+            response = sns.publish(
                 TopicArn=TOPIC_ARN,
                 Subject="Event Reminder",
                 Message=f"""
@@ -57,10 +70,19 @@ Time: {event_item['time']}
 """
             )
 
+            print("SNS Response:")
+            print(response)
+
             event_item["announced"] = True
             updated = True
 
+        else:
+
+            print("Not time yet.")
+
     if updated:
+
+        print("Updating events.json...")
 
         s3.put_object(
             Bucket=BUCKET_NAME,
@@ -68,6 +90,14 @@ Time: {event_item['time']}
             Body=json.dumps(events, indent=2),
             ContentType="application/json"
         )
+
+        print("events.json updated.")
+
+    else:
+
+        print("No updates made.")
+
+    print("========== Finished ==========")
 
     return {
         "statusCode": 200,
